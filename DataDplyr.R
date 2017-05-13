45153fbe82ebdedb80f1996c90a6e2b11b2bbf11
amir <amir.meshkati@gmail.com>
2017-05-13 22:04:28 +0200

    No comment

--- DataDplyr.R
+++ DataDplyr.R
@@ -10,3 +10,12 @@ qplot(Weeklydf$WeekNr,Weeklydf$Abverkauf)+geom_line()
 ggplot(Weeklydf, aes(Weeklydf$WeekNr))+ 
   geom_line(aes(y=Weeklydf$Abverkauf))+
   geom_line(aes(y=200+50*Weeklydf$WerbungMax,color = 'red'))
+
+df <- MyDF
+ggplot(df, aes(df[1]))+ 
+  geom_line(aes(y=df[2]))#+
+  geom_line(aes(y=200+50*df[3],color = 'red'))
+
+
+qplot(df[1],df$Datum)
+head(MyDF[2])
