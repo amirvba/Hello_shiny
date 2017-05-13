543a46d2fd115b23d46adde072a235b91957b4f1
amir <amir.meshkati@gmail.com>
2017-05-13 16:14:44 +0200

    Update Ideas.R

--- Ideas.R
+++ Ideas.R
@@ -1,4 +1,4 @@
 # Use features like AM and PM
 # WHat about weekly data
 # How does seem like if I delete non usefull ouliers?
-# Use smaller names for directories
+# Use smaller names for directories!
