a8ec76f748f806f2e0b536bea4664bffc480140c
amir <amir.meshkati@gmail.com>
2017-05-14 19:19:22 +0200

    I draw my first Regression plot. I hate it. But I learned a lot about how important it is to use good features!

--- DataDplyr.R
+++ DataDplyr.R
@@ -21,14 +21,19 @@ mylm <- lm(df$Abverkauf~df$Datum+df$WerbungMax)
 df$Fitted <- mylm$fitted.values
 summary(mylm)
 
+n <- 4
+FittedMA <- c(rep(NA,n)  ,head(rollmean(df$Abverkauf, n),-1))
+df$FittedMA <- FittedMA
+df %>% head()
+
 qplot(df$Datum, df$Fitted)
 qplot(df$Datum, df$Abverkauf)#+geom_line()#,type="l")
 
 ggplot(df, aes(df$Datum))+ 
   geom_line(aes(y=df$Abverkauf))+
-  geom_line(aes(y=df$Fitted,color = 'red'))
+  geom_line(aes(y=df$FittedMA,color = 'red'))
 
 
 ggplot(df, aes(df$WeekNr))+ 
   geom_line(aes(y=df$Abverkauf))+
-  geom_line(aes(y=df$Fitted,color = 'red'))
+  geom_line(aes(y=df$FittedMA,color = 'red'))
