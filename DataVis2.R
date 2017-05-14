f6a3418206ad68029d630ac5aa2ddf12ded623e8
amir <amir.meshkati@gmail.com>
2017-05-14 19:37:00 +0200

    In this script I visualise the Fitted Values from algorithms which I use.

---
+++ DataVis2.R
@@ -0,0 +1,25 @@
+n <- 7
+df <- MyDF %>% get__PMdf() %>% get__Weeklydf()
+
+mylm <- lm(df$Abverkauf~ df$WeekNr+df$WerbungMax)
+df$FittedReg <- mylm$fitted.values
+summary(mylm)
+
+
+
+FittedMA <- c(rep(NA,n)  ,head(rollmean(df$Abverkauf, n),-1))
+df$FittedMA <- FittedMA
+df %>% head()
+
+qplot(df$WeekNr, df$FittedMA)+geom_line()
+qplot(df$WeekNr, df$Abverkauf)+geom_line()#,type="l")
+
+ggplot(df, aes(df$Datum))+ 
+  geom_line(aes(y=df$Abverkauf))+
+  geom_line(aes(y=df$FittedMA,color = 'red'))
+
+
+ggplot(df, aes(df$WeekNr))+ 
+  geom_line(aes(y=df$Abverkauf))+
+  geom_line(aes(y=df$FittedMA, color = 'red'))+
+  geom_line(aes(y=df$FittedReg, color = 'blue'))
